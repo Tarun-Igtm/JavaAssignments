b4d330ca2c9782273364a0766ca9d071f581c97f
Tarun Kumawat <kumawattarun068@gmail.com>
2025-07-02 14:09:58 +0530

    Added clean .java files

--- DecodeLogic.java
+++ DecodeLogic.java
@@ -1,24 +1,13 @@
-import java.util.Scanner;
-
-public class DecodeLogic {
+public class LNBDecodeLogic {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
+        int a = 10;
+        int b = 20;
         int product = a * b;
 
         if (product > 500) {
-            int sum = a + b;
-            System.out.println("Product is greater than 500. Sum is: " + sum);
+            System.out.println("Sum is: " + (a + b));
         } else {
-            System.out.println("Hello LNB code is running fine !!");
+            System.out.println("Hello LNB code is running fine!!");
         }
-
-        sc.close();
     }
 }
